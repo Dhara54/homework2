84f548e106bc84c068c8d1a998193b8f1704ed45
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-25 23:13:25 -0400

    new

--- oop.php
+++ oop.php
@@ -1,27 +1,3 @@
 <?php
-
-
-echo "<h1>String length</h1>";
-echo "web system development";
-echo strlen(“web system development”);
-
-echo str_word_count(“Welcome to Cloudways”);
-echo strrev(“Welcome to Cloudways”);
-echo str_replace(“cloudways”, “the programming world”, “Welcome to cloudways”);
-echo ucwords(“welcome to the php world”);
-echo strtoupper(“welcome to cloudways”);
-echo str_repeat(“=”,13);
-
-echo strcmp(“Cloudways”,”Hosting”);
-echo substr(“Welcome to Cloudways”,0,10).”<br>”;
-
-$str = “Wordpess Hosting”;
-echo $str . “<br>”;
-echo trim(“$str”,”Wording”);
-
-}
-
-
-
+echo "Hello";
 ?>
-
