a912ab4979808a6332992d0a1f6f3a71e76a7827
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-26 17:26:12 -0400

    fg

--- oop.php
+++ oop.php
@@ -14,13 +14,27 @@ echo str_word_count($str);
 echo "</br> Reverse of the string is : ";
 echo strrev($str);
 
-
-
 echo "</br> This will convert first character of each word in a string to uppercase : ";
 echo ucwords($str);
 
 echo "</br> This will convert string to uppercase : ";
 echo strtoupper($str);
+
+echo "</br>This will convert string to lowercase : ";
+strtolower($str);
+
+echo "</br>The string is reapeating for 10 times : ";
+echo str_repeat($str,10);
+
+
+echo "</br>This is comparison of two string : ";
+echo "</br>First string is : Welcome ";
+echo "</br>Second string is : to the php ";
+echo strcmp(“ Welcome ”,” to the php ”);
+
+echo "</br> This wil cut the string in parts : ";
+echo substr(“Welcome to the php”,0,10).”;
+
 
 
 ?>
