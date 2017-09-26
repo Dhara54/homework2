0fbcf40bf9257bcd527c66663c610355669c5224
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-26 01:23:32 -0400

    five

--- oop.php
+++ oop.php
@@ -4,8 +4,9 @@
 <?php
 
 echo "Hello";
-echo str_word_count(“Welcome to Cloudways”);
-echo strrev(“Welcome to Cloudways”);
+$str= "this is dhara";
+echo str_word_count(“$str”);
+echo strrev(“$str”);
 
 
 
