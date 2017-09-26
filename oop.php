4c857c981d23a95873bfb9c0045bee0fe759641b
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-26 17:05:16 -0400

    lk

--- oop.php
+++ oop.php
@@ -14,8 +14,6 @@ echo str_word_count($str);
 echo "</br> Reverse of the string is : ";
 echo strrev($str);
 
-echo "</br>This will replace string : ";
-echo str_replace(“php”, “the programming world”, “Welcome”);
 
 
 echo "</br> This will convert first character of each word in a string to uppercase : ";
