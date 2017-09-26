92b4d4bfcaa0711c1530071abc19bc703390c1f7
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-26 17:02:59 -0400

    jk

--- oop.php
+++ oop.php
@@ -7,13 +7,22 @@ echo "Hello</br>";
 $str= "Welcome to php";
 echo "Welcome to php</br>";
 
-echo "</br>lenght of the string";
+echo "</br>lenght of the string is : ";
 echo strlen($str);
-echo"</br>word count of the string";
+echo"</br>word count of the string is : ";
 echo str_word_count($str);
-echo "</br> Reverse of the string";
+echo "</br> Reverse of the string is : ";
 echo strrev($str);
 
+echo "</br>This will replace string : ";
+echo str_replace(“php”, “the programming world”, “Welcome”);
+
+
+echo "</br> This will convert first character of each word in a string to uppercase : ";
+echo ucwords($str);
+
+echo "</br> This will convert string to uppercase : ";
+echo strtoupper($str);
 
 
 ?>
