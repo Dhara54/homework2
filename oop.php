8cb1ed683f87cf712918b7a2539468ac666a2265
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-09-26 01:33:14 -0400

    seven

--- oop.php
+++ oop.php
@@ -3,9 +3,15 @@
 
 <?php
 
-echo "Hello";
-$str= "this is dhara";
+echo "Hello</br>";
+$str= "Welcome to php";
+echo "Welcome to php</br>";
+
+echo "</br>lenght of the string";
+echo strlen($str);
+echo"</br>word count of the string";
 echo str_word_count($str);
+echo "</br> Reverse of the string";
 echo strrev($str);
 
 
