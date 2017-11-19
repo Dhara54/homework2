4b588a07f1e1b4007e585172058e8510804b0ca0
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-11-19 10:24:25 -0500

    database connection

--- week8/conn.php
+++ week8/conn.php
@@ -8,10 +8,10 @@ define('PASSWORD', 'ewKtFdfg');
 define('CONNECTION', 'sql1.njit.edu');
 
 class dbConn{
-    //variable to hold connection object.
+    //variable to hold connection object
     protected static $db;
    
-    //private construct - class cannot be instatiated externally.
+    //private construct - class cannot be instatiated externally
     private function __construct() {
       
         try {
@@ -20,18 +20,19 @@ class dbConn{
             self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         }
         catch (PDOException $e) {
-            //Output error - would normally log this to error file rather than output to user.
+            //Output error - would normally log this to error file rather than output to user
             echo "Connection Error: " . $e->getMessage();
         }
     }
     // get connection function. Static method - accessible without instantiation
-    public static function getConnection() {
-        //Guarantees single instance, if no connection object exists then create one.
+    public static function getConnection() 
+    {
+        //Guarantees single instance, if no connection object exists then create one
         if (!self::$db) {
-            //new connection object.
+            //new connection object
             new dbConn();
         }
-        //return connection.
+        //return connection
         return self::$db;
     }
 }
